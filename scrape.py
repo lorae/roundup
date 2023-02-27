394b9befeb03aaa3513f13ab0378d9ff35908f83
lorae-brookings <124624026+lorae-brookings@users.noreply.github.com>
2023-02-27 17:50:45 -0500

    scrape.py now scrapes BOE RSS feed!
    Also, I added its output into a new folder called throughput\BOE.csv.
    Python Tutorialv2.docx also has some changes!

--- scrape.py
+++ scrape.py
@@ -1,75 +1,63 @@
 # Lorae Stojanovic
-# LE: 21 Nov 2022
+# Special thanks to ChatGPT for coding assistance in this project.
+# LE: 27 Feb 2023
 # https://realpython.com/beautiful-soup-web-scraper-python/
+
 import requests
-from beautifulsoup4 import BeautifulSoup
+from bs4 import BeautifulSoup
 import feedparser
-import pandas as pd # for data frame
-#from itertools import compress # for compress function
+import pandas as pd
+
+# Define functions
+# Define functions
+def get_soup(url):
+    page = requests.get(url) # Get the HTML content of the page at the current link
+    soup = BeautifulSoup(page.content, 'html.parser') # Parse the HTML content using BeautifulSoup
+    return soup
+
+def get_element(df, link, index):
+    return [get_soup(link) # Get the HTML content of the page at the current link and parse it using BeautifulSoup
+            .find('div', {'class': 'page-content'})  # Find the 'div' tag with class 'page-content'
+            .text # Extract the text of the 'div' tag
+            .strip() # Remove any leading/trailing whitespace from the text
+            .split('\n')[index]] # Split the text by newline character '\n' and return the third element (the abstract)
+
+def get_abstracts(df):
+    return [get_element(df, link, 2) for link in df['Link']]
+
+def get_authors(df):
+    return [get_element(df, link, 1) for link in df['Link']]
 
 '''
-Everything but the abstract and authors are in the RSS feed, so to simplify
-life  we'll get all of that data directly from the feed.
-"Sum" and "Use" are only temporarily needed to indicate which parts
-of the RSS feed are working papers and which are other things (quarterly
-reports, etc))
+Note to self: I can make this third function cleaner by trying to leverage the fact it is almost the same
+as get_element
 '''
+def get_numbers(df):
+    return [get_soup(link) # Get the HTML content of the page at the current link and parse it using BeautifulSoup
+            .find('div', {'class': 'page-content'})  # Find the 'div' tag with class 'page-content'
+            .text # Extract the text of the 'div' tag
+            .strip() # Remove any leading/trailing whitespace from the text
+            .split('\n')[0] # Split the text by newline character '\n' and return the third element (the abstract)
+            .replace("Staff Working Paper No. ", "") # remove unnecessary text
+            .replace(",", "") # remove unnecessary commas
+            for link in df["Link"]]  # Iterate over each link in the dataframe
+
+# Main code
 URL = "https://www.bankofengland.co.uk/rss/publications"
 f = feedparser.parse(URL)
-Title = []
-Link = []
-Date = []
-Sum = []
-Use = []
-for n in range(0, len(f.entries)):
-    title = (f.entries[n].title)
-    link = (f.entries[n].link)
-    sum = (f.entries[n].summary)
-    pub_date = (f.entries[n].published)
-    if "working paper" in sum:
-        Use.append(True)
-    else:
-        Use.append(False)
-    Title.append(title)
-    Link.append(link)
-    Sum.append(sum)
-    Date.append(pub_date)
-'''
-Now we've extracted titles and links. To use only the links that say 
-"working-paper" in their URL, we'll use the compress() function.
-'''
-def choose(x, use):
-    output = list(compress(x, use))
-    return output
 
-Title = choose(Title, Use)
-Link = choose(Link, Use)
-Date = choose(Date, Use)
+data = [(entry.title,
+         entry.link,
+         entry.published) #creates a tuple containing the title, link, publication date, and summary for the current entry
+        for entry in f.entries #introduce a loop that iterates over each entry in the RSS feed (f.entries)
+        if "working paper" in entry.summary] #filters the entries based on whether the phrase "working paper" appears in the summary
 
-'''
-Now we need the abstract and authors. For that, we have to individually
-navigate to each link.
-'''
-#def get_abstract(url):
-    #read url
-    #extract entire container "div.page-content". Necessary since each container
-    #is different
-    #parse text,
-    #split by "\n" marker
-    #grab third element
-#def get_author(url):
-    #read url
-    #extract entire container "div.page-content". Necessary since each container
-    #is different
-    #parse text,
-    #split by "\n" marker
-    #grab third element
-Abstract = []
-Author = []
-for m in range(0, len(Link)):
-    link = (Link[m])
+df = pd.DataFrame(data, columns=["Title", "Link", "Date"])
+df["Abstract"] = get_abstracts(df)
+df["Author"] = get_authors(df)
+df["Number"] = get_numbers(df)
 
-print(Link[1])
-# df1 = pd.DataFrame(lst, columns=cols)
-# print(df1)
+print(df)
+# This line is buggy and doesn't output properly. But within Python, the output is good.
+df.to_csv('throughput/BOE.csv', index=False)
 
