7c534137681e9f4fb5f9fd92df0257774a48b15f
Lorae <stojanovic.lorae@gmail.com>
2023-03-02 17:01:17 -0500

    Improved NBER.py
    - NBER.py now scrapes last 100 entries on NBER API
    - requirements.txt updated to reflect lxml package
    - very minor change to Python Tutorial v2

--- scripts/NBER.py
+++ scripts/NBER.py
@@ -5,22 +5,37 @@
 import requests
 import re
 import json
+import pandas as pd
+from lxml import html
 
 url = 'https://www.nber.org/api/v1/working_page_listing/contentType/working_paper/_/_/search?page=1&perPage=100'
 response = requests.get(url)
 data = json.loads(response.text)
 
-title = data['results'][0]['title']
-authors_html = data['results'][0]['authors']
-authors = [re.sub('<[^>]+>', '', author) for author in authors_html]
-published_date = data['results'][0]['displaydate']
-abstract = data['results'][0]['abstract']
-url = "https://www.nber.org" + data['results'][0]['url']
-number = url.split('/papers/w')[1] # extract the paper number
+titles = [paper['title'] for paper in data['results']]
+authors_htmls = [paper['authors'] for paper in data['results']]
+authors = [[re.sub('<[^>]+>', '', author) for author in authors_html] for authors_html in authors_htmls]
+published_dates = [paper['displaydate'] for paper in data['results']]
+abstracts = [paper['abstract'] for paper in data['results']]
+urls = ["https://www.nber.org" + paper['url'] for paper in data['results']]
+numbers = [url.split('/papers/w')[1] for url in urls]
 
-print(title)
-print(authors)
-print(published_date)
-print(abstract)
-print(url)
-print(number)
+long_abstracts = []
+for url in urls:
+    page = requests.get(url)
+    tree = html.fromstring(page.content)
+    long_abstract = tree.xpath('/html/body/div[2]/main/div[2]/div[2]/div/p/text()')[0].strip()
+    long_abstracts.append(long_abstract)
+
+df = pd.DataFrame({
+    'Title': titles,
+    'Authors': authors,
+    'Published Date': published_dates,
+    'Abstract': abstracts,
+    'URL': urls,
+    'Number': numbers,
+    'Long Abstract': long_abstracts
+})
+
+df = df.sort_values(by=['Number'])
+print(df)
