e0ae1eda15405b55ae67b5dba773057878558579
Lorae <stojanovic.lorae@gmail.com>
2023-03-03 15:16:33 -0500

    Fixed IMF further
    It works as it should, but I think the code can be more efficient, so I will branch it

--- scripts/IMF.py
+++ scripts/IMF.py
@@ -13,31 +13,72 @@ from bs4 import BeautifulSoup
 import feedparser
 import pandas as pd
 from lxml import html
+import re
 
-# Define functions
-def get_element_text(url, xpath):
-    response = requests.get(url)
-    tree = html.fromstring(response.content)
-    elems = tree.xpath(xpath)
-    if isinstance(elems, list):
-        return ''.join([elem.text_content().strip() for elem in elems])
-    elif elems is not None:
-        return elems.text.strip()
-    else:
-        return None
+def get_element(df, x):
+    # Define an empty list to hold the elements
+    elements = []
+    # Iterate over each link in the input DataFrame
+    for link in df['Link']:
+        # Make an HTTP request to the link and parse the HTML content
+        response = requests.get(link)
+        tree = html.fromstring(response.content)
+        # Extract the element from the HTML content and append it to the list
+        element = tree.xpath(x)
+    # Return the list of elements
+    return elements
 
-
+    
 def get_abstracts(df):
-    return [get_element_text(link, '/html/body/div[3]/main/article/div[1]/div/section[1]/p[8]/text()')
-            for link in df['Link']]
+    # Define an empty list to hold the abstracts
+    abstracts = []
+    # Iterate over each link in the input DataFrame
+    for link in df['Link']:
+        # Make an HTTP request to the link and parse the HTML content
+        response = requests.get(link)
+        tree = html.fromstring(response.content)
+        # Extract the abstract from the HTML content and append it to the list of abstracts
+        abstract = tree.xpath('/html/body/div[3]/main/article/div[1]/div/section[1]/p[8]/text()')[0]
+        abstracts.append(abstract)
+    # Return the list of abstracts
+    return abstracts
+
 
 def get_authors(df):
-    return [[author.text for author in BeautifulSoup(requests.get(link).content, 'html.parser').select_one('p.pub-desc.hide').find_all('a')]
-            for link in df['Link']]
+    # Define an empty list to hold the list of authors for each link
+    authors_list = []
+    # Iterate over each link in the input DataFrame
+    for link in df['Link']:
+        # Make an HTTP request to the link and parse the HTML content
+        response = requests.get(link)
+        tree = html.fromstring(response.content)
+        # Extract the list of author elements from the HTML content
+        author_elems = tree.xpath('/html/body/div[3]/main/article/div[1]/div/section[1]/p[2]')
+        # Clean up the author names and separate them with commas
+        authors = [re.sub(r'\s+', ' ', author.text_content()) for author in author_elems]
+        authors_comma_sep = [re.sub(r'\s*;\s*', ', ', author) for author in authors]
+        # Append the list of cleaned up and comma-separated author names to the authors_list
+        authors_list.append(authors_comma_sep)
+    # Return the list of authors for each link
+    return authors_list
+
 
 def get_numbers(df):
-    return [get_element_text(link, '/html/body/div[3]/main/article/div[1]/div/section[3]/div/p[6]')
-            for link in df['Link']]
+    # Define an empty list to hold the numbers
+    numbers = []
+    # Iterate over each link in the input DataFrame
+    for link in df['Link']:
+        # Make an HTTP request to the link and parse the HTML content
+        response = requests.get(link)
+        tree = html.fromstring(response.content)
+        # Extract the numbers element from the HTML content
+        numbers_elem = tree.xpath('/html/body/div[3]/main/article/div[1]/div/section[3]/div/p[6]')[0]
+        # Extract the text content of the numbers element and remove any leading/trailing whitespace
+        numbers_text = numbers_elem.text_content().strip()
+        # Append the cleaned up numbers to the numbers list
+        numbers.append(numbers_text)
+    # Return the list of numbers
+    return numbers
 
 # Main code
 URL = "https://www.imf.org/en/Publications/RSS?language=eng&series=IMF%20Working%20Papers"
@@ -45,14 +86,19 @@ f = feedparser.parse(URL)
 
 data = [(entry.title,
          entry.link,
-         entry.published) # creates a tuple containing the title, link, publication date, and summary for the current entry
+         entry.published) #creates a tuple containing the title, link, publication date, and summary for the current entry
         for entry in f.entries]
 
 df = pd.DataFrame(data, columns=["Title", "Link", "Date"])
+print("IMF titles, links, and dates have been gathered.")
+
 
 df["Abstract"] = get_abstracts(df)
+print("... abstracts have been gathered.")
 df["Author"] = get_authors(df)
+print("... authors have been gathered.")
 df["Number"] = get_numbers(df)
+print("... numbers have been gathered.")
 
 print(df)
 
