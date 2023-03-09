ea68eed04c0371a3ebe0a2d19ffba714b99f71e7
lorae-brookings <124624026+lorae-brookings@users.noreply.github.com>
2023-03-09 10:38:51 -0500

    BFI now works well!
    Might make the code slightly more concise, but it works well!

--- scripts/BFI.py
+++ scripts/BFI.py
@@ -13,31 +13,52 @@ url = "https://bfi.uchicago.edu/working-papers/"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 
-Title = [a.text for a in soup.select('.teaser__title a')]
-print(Title)
-Date = [pd.to_datetime(date.get_text(strip=True), format='%b %d, %Y').strftime('%B %d, %Y') for date in soup.select('.meta__date')]
-print(Date)
-Author = [name.get_text(strip=True) for name in soup.select('.person-list__names')]
-print(Author)
-#The links don't work properly
-Link = [a['href'] for a in soup.select('a.button.clear') if 'https://bfi.uchicago.edu/working-paper/' in a['href']]
-print(Link)
+# Initialize the lists
+titles = []
+links = []
+dates = []
+abstracts = []
+authors = []
+numbers = []
 
+# Get titles, links, dates, and authors from the main website. Note
+# that BFI papers are not numbered, so "numbers" will be populated
+# with NA.
+for element in soup.select('div.teaser.teaser--working-paper'):
+    title = element.select('h2.teaser__title')[0].text.strip()
+    titles.append(title)
+    
+    link = element.select('h2.teaser__title a')[0]['href']
+    links.append(link)
+    
+    date = element.select('span.meta__date')[0].text.strip()
+    dates.append(date)
+    
+    author = element.select('div.teaser__names')[0].text.strip()
+    authors.append(author)
+    
+    number = 'NA' #BFI doesn't number their working papers
+    numbers.append(number)
 
-def get_abstract(link):
+# Navigate to each page's link to get the abstracts
+for link in links:
     page = requests.get(link)
     soup = BeautifulSoup(page.content, 'html.parser')
-    abstract = soup.select_one('div[itemprop="articleBody"] p').get_text(strip=True)
-    return abstract
+    abstract = soup.select('div.textblock')[0].text.strip()
+    abstracts.append(abstract)
 
-Abstract = [get_abstract(link) for link in Link]
+# create a dictionary of the six lists, where the keys are the column names
+data = {'Title': titles, 'Link': links, 'Date': dates, 'Author': authors, 'Number': numbers, 'Abstract': abstracts}
 
-BFI = pd.DataFrame({
-    'Title': Title,
-    'Author': Author,
-    'Date': Date,
-    'Abstract': Abstract,
-    'Link': Link,
-    'Source': 'BFI'
-})
-BFI.to_pickle('output/BFI.pkl')  # Save as a pickle file instead of RDS
+# create a DataFrame from the dictionary
+df = pd.DataFrame(data)
+
+# save the data frame to a JSON file
+df.to_json('../processed_data/BFI.json', orient='records')
+print("df saved to json")
+
+# load the data frame from the JSON file
+df_loaded = pd.read_json('../processed_data/BFI.json')
+print("df_loaded loaded from json")
+    
+    
