e4d0c34dc4fb2a477d7287f8110d5e65e2aef375
Lorae <stojanovic.lorae@gmail.com>
2023-08-04 23:59:59 -0400

    push of the day
    dallas is getting there. It's a tricky one.

--- roundup_scripts/scrapers/Fed_Dallas.py
+++ roundup_scripts/scrapers/Fed_Dallas.py
@@ -2,40 +2,147 @@
 # The purpose of this script is to scrape metadata from the most recent Cleveland Fed working papers. This script uses
 # the Cleveland Fed's Working Paper landing page.
 #
-# Note from Lorae: This webpage is awesome. If you just scrape the url below, it gives you every single working paper
-# ever published. Yes, it is dynamically rendered using Java - but all the underlying data is obtainable with one, 
-# simple scrape. My favorite webpage so far to scrape.
-
-# Never mind. Note for future self:
-# webpage has groups of empty <div> tags that separate each entry.
-# each set of <div> tags has an <h3> and then USUALLY 3 <p> entries under it
-# <h3> contains the number
-# first <p> contains title, link (to the pdf, not landing page), and abstract
-# second ,p. contains more crap
-# more crap
-# last p contains the doi (which I want instead of the href on the title)
+# This code is still buggy. It doesn't always get a clean cut of the abstract or the authors for entries where there is
+# only one p tag (typically it is a revision of a previous paper).
 #
 # Lorae Stojanovic
 # Special thanks to ChatGPT for coding assistance in this project.
 # LE: 4 Aug 2023
 
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Comment
 import pandas as pd
+import re
 
 def get_soup(url): # Used to get the initial soup from the main URL that lists all the papers
     # In order for the code to run, it is necessary to spoof a browser. Otherwise, the website will not provide the information
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0"}
     page = requests.get(url, headers=headers) # Include headers in request
     soup = BeautifulSoup(page.content, 'html.parser') # Parse the HTML content using BeautifulSoup
+    # Find all comment tags
+    comments = soup.findAll(text=lambda text: isinstance(text, Comment))
+
+    # Remove all comment tags from the soup
+    for comment in comments:
+        comment.extract()
     return soup
 
 url = "https://www.dallasfed.org/research/papers"
 
 soup = get_soup(url)
 
+# First, find all the h3 tags
+h3_tags = soup.find_all('h3')
+
+count = 0
+for h3 in h3_tags:
+    # Stop processing if we have already processed 20 entries
+    if count >= 100:
+        break
+
+    # Get the number from the h3 tag
+    number = h3.text
+    # Check if 'Globalization Institute' is in the number string. Replace with "GI"
+    if 'Globalization Institute' in number:
+        number = number.replace('Globalization Institute No. ', 'GI')
+    print(number)
+    
+    # Navigate to the next p tag
+    next_p = h3.find_next_sibling('p')
+    # Get the title, abstract, and authors
+    title = next_p.find('strong').text if next_p.find('strong') else None
+    print(title)
+    # There's many ways to get the abstract but I find splicing the string is the simplest,
+    # easiest strategy in this situation.
+    abstract = next_p.text.split("Abstract:")[1].strip() #everything appearing after "Abstract:" is the abstract
+    abstract = abstract.split("DOI")[0].strip() # cut off anything containing a DOI
+    print(abstract)
+    print(":)")
+    # The authors always appear after the title and before the "Abstract: " string. But sometimes,
+    # there is also an "Appendix" string that has to be stripped away.
+    author = next_p.text.split("Abstract:")[0].replace(title, "")
+    author = author.replace("Appendix", "").replace("Code", "").strip()
+    print(author)
+    
+    # Navigate to the last p tag before the next div tag.
+    # Find the next div tag anywhere in the document after the h3 tag.
+    next_div = h3.find_next('div')
+    if next_div is None:
+        print("doi not found")
+    else:
+        last_p = next_div.find_previous('p')
+        doi_string = last_p.get_text()
+    
+    # If there only happens to be one p tag, we remove the abstract
+    # to prevent confusion or duplicate information.
+    if last_p == next_p:
+        doi_string = last_p.get_text().replace(abstract, "")
+        
+    # This regular expression pattern should match most DOIs
+    doi_pattern = r"(^|\n)DOI:\s*https://doi\.org/\S+"
+    match = re.search(doi_pattern, doi_string)
+    if match:
+        link = match.group(0).replace("DOI:", "").strip()
+    else:
+        link = "DOI not found"
+    print(link)
+    
+    print(".............")
+    print(" ")
+        
+    count += 1
 
 
+'''
+        print(title)
+    # Now we'll loop through the siblings of the h3 tag (i.e., the tags that follow it at the same level)
+    for sibling in h3.find_next_siblings():
+        print(h3.find_next_siblings())
+        if sibling.name == 'p':
+            # This is one of the p tags we're interested in
+            # This is the first p tag, containing the title, link, and abstract
+            title = sibling.find('strong').text if sibling.find('strong') else None
+            #link = sibling.find('a')['href'] if sibling.find('a') else None
+            abstract = sibling.text  # Or use any other method if the text is structured differently
+            
+            print(title)
+            #print(link)
+            print(abstract)
+            print(".............")
+            print(" ")
+            
+        elif sibling.name == 'div':
+            # This is a div tag, signifying the end of the current entry
+            # Increase the count and break the inner loop
+            count += 1
+            break
+
+    
+
+    # Initialize some variables to store the title, link, abstract, and DOI
+    title = link = abstract = doi = None
+
+    # Now we'll loop through the siblings of the h3 tag (i.e., the tags that follow it at the same level)
+    for sibling in h3.find_next_siblings():
+        if sibling.name == 'p':
+            # This is one of the p tags we're interested in
+
+            if title is None:
+                # This is the first p tag, containing the title, link, and abstract
+                title = sibling.find('strong').text if sibling.find('strong') else None
+                link = sibling.find('a')['href'] if sibling.find('a') else None
+                abstract = sibling.text  # Or use any other method if the text is structured differently
+
+            elif 'DOI' in sibling.text:
+                # This is the last p tag, containing the DOI
+                doi = sibling.text  # Or use any other method if the text is structured differently
+
+        elif sibling.name == 'div':
+            # This is a div tag, signifying the end of the current entry
+            break
+
+    # Now you can do what you want with paper_number, title, link, abstract, and doi
+'''
 
 
 
