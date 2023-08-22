da91010676938face8bac40dba2518cf61fadb3c
Lorae <stojanovic.lorae@gmail.com>
2023-08-22 13:52:03 -0400

    Update Fed_Boston.py
    I now have it scraping metadata from pdfs to get the publication date. But Fed_Boston.py is not yet finished.

--- roundup_scripts/scrapers/Fed_Boston.py
+++ roundup_scripts/scrapers/Fed_Boston.py
@@ -8,9 +8,15 @@
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import time
+import requests
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import PyPDF2
+from io import BytesIO
+import io
+
 
 def get_soup(url): 
     # Create a new instance of the Firefox driver
@@ -25,11 +31,11 @@ def get_soup(url):
     )
     button.click()
 
-    # If you need to wait for some elements to ensure the page has loaded after clicking, 
-    # you can use WebDriverWait. For now, I'll add a simple wait to check for page complete loading.
+    # Wait until a certain number of 'event-list-item' divs appear. Adjust the number '10' as needed.
     WebDriverWait(driver, 30).until(
-        lambda driver: driver.execute_script('return document.readyState') == 'complete'
+        EC.presence_of_all_elements_located((By.CLASS_NAME, 'event-list-item'))
     )
+    time.sleep(5)
 
     # Get the page source and parse it
     soup = BeautifulSoup(driver.page_source, 'html.parser')
@@ -39,9 +45,85 @@ def get_soup(url):
 
     return soup
     
+def extract_pdf_metadata_from_url(pdf_url):
+    # Download the PDF
+    response = requests.get(pdf_url)
+    response.raise_for_status()  # Raise an exception for HTTP errors
+
+    # Use BytesIO to convert the downloaded content to a file-like object so it can be read by PyPDF2
+    with BytesIO(response.content) as pdf_file:
+        # Create a PDF reader object
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
+        
+        # Extract metadata
+        metadata = pdf_reader.metadata
+
+    return metadata
+    
 url = "https://www.bostonfed.org/publications/research-department-working-paper/"
 soup = get_soup(url)
 #print(soup)
 
-elements = soup.find_all('div', {'class': 'event-list-item'})
-print(elements)
+elements = soup.find('div', {'class': 'event-list'}).find_all('div', {'class': 'event-body-wrapper'})
+#print(elements)
+
+for el in elements:
+    title = el.find('h2', {'class': 'card-title'}).find('a')['title']
+    print(title)
+    
+    link = "https://www.bostonfed.org" + el.find('h2', {'class': 'card-title'}).find('a')['href']
+    print(link)
+    
+    # now we get the rest.
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0"}
+    page = requests.get(link, headers=headers) # Include headers in request
+    soup = BeautifulSoup(page.content, 'html.parser') # Parse the HTML content using BeautifulSoup
+    
+    authors = soup.find('div', {'class': 'row working-paper-by-author-row'}).get_text().strip().replace("By ", "")
+    print(authors)
+    
+    abstract = soup.find('div', {'id': 'collapse3'}).get_text().strip()
+    print(abstract)
+    
+    number = soup.find('p', {'class': 'doi-text'}).get_text().split("No. ")[1].split("https:")[0].replace(".", "").strip()
+    print(number)
+
+
+
+
+# Example usage
+pdf_url = 'https://www.bostonfed.org/-/media/Documents/Workingpapers/PDF/2023/wp2308.pdf'
+metadata = extract_pdf_metadata_from_url(pdf_url)
+
+for key, value in metadata.items():
+    print(f"{key}: {value}")
+
+from datetime import datetime
+
+def extract_and_format_moddate(metadata):
+    # Extract the ModDate string
+    mod_date_str = metadata['/ModDate'][2:16]  # Extracts '20230303104258' from 'D:20230303104258-05'00''
+    
+    # Parse the ModDate string into a datetime object
+    mod_date = datetime.strptime(mod_date_str, '%Y%m%d%H%M%S')
+    
+    # Format the datetime object in the desired format
+    formatted_date = mod_date.strftime('%b %d, %Y')
+    
+    return formatted_date
+
+
+formatted_mod_date = extract_and_format_moddate(metadata)
+print(formatted_mod_date)  # Outputs: Mar 03, 2023
+
+# This webpage is horrible to parse. Not working.
+'''
+for el in elements:
+    authors = el.find('p', {'class': 'pub-card-authors'}).find_all('a', {'ng-href': True})
+    for a in authors:
+        author = a.get_text()
+        print(author)
+#elements = soup.find_all('div', {'class': 'event-list-item'})
+#print(elements)
+'''
+
