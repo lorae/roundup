7c5b29ac86a43f4c5f19cb01608d681e5a6950a8
Lorae <stojanovic.lorae@gmail.com>
2023-10-31 15:16:27 -0400

    Make the Streamlit app look nicer

--- roundup_scripts/streamlit_app.py
+++ roundup_scripts/streamlit_app.py
@@ -12,12 +12,25 @@ csv_url = 'https://raw.githubusercontent.com/lorae/roundup/main/historic/papers-
 # Read and display the CSV file
 df = pd.read_csv(csv_url)
 
-# Drop-down menu for sorting
-sort_option = st.selectbox('Sort by:', options=['None'] + list(df.columns))
+# Remove the 'Number' column
+df = df.drop(columns=['Number'])
+
+# Create hyperlinks for the 'Title' column using the 'Link' column
+df['Title'] = df.apply(lambda row: f"[{row['Title']}]({row['Link']})", axis=1)
+
+# Remove the 'Link' column
+df = df.drop(columns=['Link'])
+
+# Split the layout into 2 columns
+col1, col2 = st.beta_columns((3,1))
+
+# Display the data table in the left column (col1)
+col1.write(df, use_container_width=True)
+
+# Drop-down menu for sorting in the right column (col2)
+sort_option = col2.selectbox('Sort by:', options=['None'] + list(df.columns))
 
 # Sort data if an option is selected
 if sort_option != 'None':
     df = df.sort_values(sort_option)
-
-# Display the data table
-st.write(df)
+    col1.write(df, use_container_width=True)  # Re-display the sorted data table
