091815debbbcde2558a8fe918f015d162df1f8ec
Lorae <stojanovic.lorae@gmail.com>
2023-10-31 16:33:13 -0400

    Update streamlit_app.py

--- roundup_scripts/streamlit_app.py
+++ roundup_scripts/streamlit_app.py
@@ -4,36 +4,45 @@
 import streamlit as st
 import pandas as pd
 
+
+
+# Cache our data
+@st.cache()
+def load_df():
+    csv_url = 'https://raw.githubusercontent.com/lorae/roundup/main/historic/papers-we-have-seen-metadata.csv'
+    df = pd.read_csv(csv_url)
+    source_options = df.Source.unique()
+
+    min_days_ago = 0
+    max_days_ago = 30 
+    
+    return df, source_options, min_days_ago, max_days_ago
+
+def check_rows(column, options):
+    return res.loc[res[column].isin(options)]
+    
 # Title of the app
 st.title('Roundup Data Viewer')
 st.header('The latest economics working papers')
 st.subheader('E pluribus unum.')
+st.markdown(html, unsafe_allow_html=True)
 html = """
 <a style='background:yellow'>Displayed are the most recent working paper publications from 18 websites.</a>
 """
-st.markdown(html, unsafe_allow_html=True)
-#st.set_page_config(layout="wide")
 
-# GitHub raw URL of the CSV file
-csv_url = 'https://raw.githubusercontent.com/lorae/roundup/main/historic/papers-we-have-seen-metadata.csv'
+df, source_options, min_days_ago, max_days_ago = load_df()
+res = df
 
-# Read and display the CSV file
-df = pd.read_csv(csv_url)
+name_query = st.text_input("String match for Name")
 
-# Remove the 'Number' column
-df = df.drop(columns=['Number'])
+if name_query != "":
+    res = res.loc[res.Name.str.contains(name_query)]
+ 
+ removal_columns = st.multiselect("Select Columns to Remove", df.columns.tolist())
+for column in removal_columns:
+    res = res.drop(column, axis=1)
+st.write(res)
 
-# Create hyperlinks for the 'Title' column using the 'Link' column
-df['Title'] = df.apply(lambda row: f"[{row['Title']}]({row['Link']})", axis=1)
-
-# Remove the 'Link' column
-df = df.drop(columns=['Link'])
-
-# Sidebar
-st.sidebar.header("Sidebar Header")
-
-# Data frame
-st.dataframe(df, height = 750)
 
 options = ["Red", "Blue", "Yellow"]
 selectbox_selection = st.selectbox("Select Color", options)
