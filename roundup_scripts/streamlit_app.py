16cde837a445bf43fb82833ee23525374e5e1444
Lorae <stojanovic.lorae@gmail.com>
2023-10-31 15:01:46 -0400

    Another try at displaying roundup data  using streamlit

--- roundup_scripts/streamlit_app.py
+++ roundup_scripts/streamlit_app.py
@@ -6,20 +6,18 @@ import pandas as pd
 # Title of the app
 st.title('Roundup Data Viewer')
 
-# File upload widget
-uploaded_file = st.file_uploader("historic/papers-we-have-seen-metadata", type=['csv'])
+# GitHub raw URL of the CSV file
+csv_url = 'https://raw.githubusercontent.com/lorae/roundup/main/historic/papers-we-have-seen-metadata.csv'
 
-# Check if a file is uploaded
-if uploaded_file is not None:
-    # Read and display the CSV file
-    df = pd.read_csv(uploaded_file)
-    
-    # Drop-down menu for sorting
-    sort_option = st.selectbox('Sort by:', options=['None'] + list(df.columns))
-    
-    # Sort data if an option is selected
-    if sort_option != 'None':
-        df = df.sort_values(sort_option)
-    
-    # Display the data table
-    st.write(df)
+# Read and display the CSV file
+df = pd.read_csv(csv_url)
+
+# Drop-down menu for sorting
+sort_option = st.selectbox('Sort by:', options=['None'] + list(df.columns))
+
+# Sort data if an option is selected
+if sort_option != 'None':
+    df = df.sort_values(sort_option)
+
+# Display the data table
+st.write(df)
