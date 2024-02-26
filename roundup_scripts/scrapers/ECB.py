d343c5beb45f91068c7f44c20a3354929bd29e92
Lorae <stojanovic.lorae@gmail.com>
2024-02-26 14:44:07 -0500

    feat: enable Chrome headless webdriver

--- roundup_scripts/scrapers/ECB.py
+++ roundup_scripts/scrapers/ECB.py
@@ -7,20 +7,36 @@
 
 from bs4 import Tag, BeautifulSoup
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.firefox.options import Options 
+
 import time
 from datetime import datetime
 import pandas as pd
 
 def get_soup(url): 
-    # Create a new instance of the Firefox driver
-    options = Options() 
-    options.add_argument("--headless") 
-    options.binary_location = "/snap/bin/firefox"
-    driver = webdriver.Firefox(options=options)
+    chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+    
+    chrome_options = Options()
+    options = [
+        "--headless",
+        "--disable-gpu",
+        "--window-size=1920,1200",
+        "--ignore-certificate-errors",
+        "--disable-extensions",
+        "--no-sandbox",
+        "--disable-dev-shm-usage"
+    ]
+    for option in options:
+        chrome_options.add_argument(option)
+
+    driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
     # Go to the page
     driver.get(url)
