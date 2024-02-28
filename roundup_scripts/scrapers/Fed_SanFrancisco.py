9037869bf9fc0c2798bcf7eb56fb8b709d877da7
Lorae <stojanovic.lorae@gmail.com>
2024-02-27 22:21:56 -0500

    docs(Fed_SanFrancisco): add clarifying comment to abstract data processing + remove outdated comment

--- roundup_scripts/scrapers/Fed_SanFrancisco.py
+++ roundup_scripts/scrapers/Fed_SanFrancisco.py
@@ -16,9 +16,8 @@ import json
 def scrape():
     # Define the URL for the SF Fed working papers API
     URL = 'https://www.frbsf.org/wp-json/wp/v2/sffed_publications?publication-type=1979&per_page=10'
-    #URL = 'https://www.frbsf.org/wp-json/wp/v2/posts'
 
-    # Make a GET request to the SF Fed working paper API and parse the JSON response into a Python dictionary
+    # Make a GET request to the SF Fed working paper API and parse the JSON response
     data = json.loads(requests.get(URL).text)
 
     # Create a Pandas DataFrame from the extracted data, with the "Long Abstract" extracted from the paper's URL using XPath
@@ -26,6 +25,9 @@ def scrape():
         'Title': [d['title']['rendered'] for d in data],
         'Link': [d['link'] for d in data],
         'Date': [d['date'].split('T')[0] for d in data], # Extract only the date, not the time
+        # Select d['content']['rendered'], which is an HTML object. Parse with Beautiful Soup and then only keep
+        # the text within the first <p> tag. (Sometimes there is a second <p> tag, usually containing information
+        # about how to download the pdf appendix).
         'Abstract': [BeautifulSoup(d['content']['rendered'], 'html.parser').find('p').text.strip() for d in data],
         'Author': [d['meta']['publication_authors'] for d in data],
         'Number': [d['id'] for d in data]
