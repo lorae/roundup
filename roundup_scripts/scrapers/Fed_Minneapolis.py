db58a75bf247c96fce07acf41a578c41ab7d6ec2
Lorae <stojanovic.lorae@gmail.com>
2024-03-01 20:03:08 -0500

    feat: create Fed_Minneapolis.py

---
+++ roundup_scripts/scrapers/Fed_Minneapolis.py
@@ -0,0 +1,4 @@
+# Fed_Minneapolis.py
+# The purpose of this script is to scrape metadata from the most recent Minneapolis Fed working papers,
+# found at https://www.xxxxxxxx. This script uses xxx to do yyy.
+#
