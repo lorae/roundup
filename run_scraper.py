adbb936884f8095987d1dd8b6a80b53939020125
Lorae <stojanovic.lorae@gmail.com>
2024-04-01 18:24:16 -0400

    feat(run_scraper): dynamically instantiate scrapers

--- run_scraper.py
+++ run_scraper.py
@@ -1,6 +1,20 @@
 from src.scraper.sites.bea_scraper import BEAScraper
+from src.scraper.sites.bfi_scraper import BFIScraper
 
-if __name__ == "__main__":
-    scraper = BEAScraper()  # Instantiate the scraper
-    df = scraper.collect_data()  # Collect the data and process it into a DataFrame
-    print(df)  # Print the DataFrame to see the result
+# List of scraper classes
+scrapers = [BEAScraper, 
+            BFIScraper]  
+
+# Progress bar 
+total_tasks = len(scrapers)
+for ScraperClass in scrapers:
+    # Append the result of each scrape to the list
+    print(f"running {ScraperClass} ...")
+    scraper = ScraperClass()  # Instantiate the scraper
+    df = scraper.collect_data()  # Collect the data and process it into a pandas df
+    print(df)
+
+'''if __name__ == "__main__":
+    scraper = BFIScraper()  # Instantiate the scraper
+    df = scraper.collect_data()  # Collect the data and process it into a pandas df
+    print(df)'''
