8d6dc9a0cf59a922e4a356f0910881417279efe8
Lorae <stojanovic.lorae@gmail.com>
2024-04-02 14:12:17 -0400

    refactor(bis_scraper): streamline data extraction from rss feed elements

--- src/scraper/sites/bis_scraper.py
+++ src/scraper/sites/bis_scraper.py
@@ -13,26 +13,29 @@ class BISScraper(GenericScraper):
         # Request and parse RSS feed contents
         f = feedparser.parse(url)
 
-        data = {
-            # Title
-            'Title': [entry.title for entry in f.entries],
-            # Link
-            'Link': [entry.link for entry in f.entries],
-            # Date
-            'Date': [entry.date.split('T')[0] for entry in f.entries],
+        data = []
+        for entry in f.entries:
+            title = entry.title
+            link = entry.link
+            date = entry.date.split('T')[0]
             # Abstract: "description" element contains both author and abstract, so grab the first element
             # after splitting the text by the line break ("<br />")
-            'Abstract': [entry.description.split('<br />')[1] for entry in f.entries],
+            abstract = entry.description.split('<br />')[1]
             # Author: "description" element contains both author and abstract, so grab the zero-th element
             # after splitting the text by the line break ("<br />"). Remove "by" from the string, then remove
             # leading and trailing spaces
-            'Author': [entry.description.split('<br />')[0].replace('by', '') for entry in f.entries],
+            author = entry.description.split('<br />')[0].replace('by', '')
             # The paper number is contained in the "link" element. Remove the leading and trailing elements
             # of the URL.
-            'Number': [entry.link.replace('https://www.bis.org/publ/work', '').replace('.htm', '') for entry in f.entries]
-        }
-
-        
+            number = entry.link.replace('https://www.bis.org/publ/work', '').replace('.htm', '')
+            data.append({
+                'Title': title,
+                'Link': link,
+                'Date': date,
+                'Abstract': abstract,
+                'Author': author,
+                'Number': number
+            })
 
         # Use the inherited process_data method to create and return the DataFrame
         return self.process_data(data)
