fb1b1ad6fb6823c58db9ebe591e9068c6bbd86ea
Lorae <stojanovic.lorae@gmail.com>
2024-04-03 20:49:26 -0400

    feat(request_json): create request_json module which produces generic API request and parses json response

--- src/scraper/get_soup.py
+++ src/scraper/get_soup.py
@@ -9,6 +9,7 @@ import time
 import requests
 from requests.exceptions import HTTPError
 from bs4 import BeautifulSoup
+from typing import Dict, Any
 
 
 def request_soup(session_args: requests.Request) -> BeautifulSoup:
@@ -77,3 +78,38 @@ def selenium_soup(url: str) -> BeautifulSoup:
     driver.quit()  # Close the WebDriver
 
     return soup
+
+
+def request_json(method: str, 
+                 url: str, 
+                 headers: Dict[str, str], 
+                 data: Dict[str, Any] = None, 
+                 files: Dict[str, Any] = None,
+                 params: Dict[str, Any] = None) -> Dict[str, Any]:
+    '''
+    Makes an HTTP request to a specified URL with the given method, headers, data, files, 
+    and query parameters, and returns the parsed JSON response.
+
+    :param method: HTTP method to use ('GET', 'POST', etc.).
+    :param url: URL to make the request to.
+    :param headers: HTTP headers to include in the request.
+    :param data: Data to send in the body of the request. Used for 'POST' and 'PUT' methods. Default is None.
+    :param files: Files to send in the multipart request. Used for 'POST' and 'PUT' methods. Default is None.
+    :param params: Query parameters to append to the URL. Used for 'GET' requests. Default is None.
+    :type method: str
+    :type url: str
+    :type headers: Dict[str, str]
+    :type data: Dict[str, Any]
+    :type files: Dict[str, Any]
+    :type params: Dict[str, Any]
+    :raises HTTPError: If the response status code is not 200.
+    :return: JSON object containing the response data.
+    '''
+    response = requests.request(method, url, headers=headers, data=data, files=files, params = params)
+    
+    # Check if the response status code is 200 (OK)
+    if response.status_code != 200:
+        raise requests.HTTPError(f'Error: Received status code {response.status_code} for URL: {response.url}', response=response)
+    
+    # Parse and return the JSON from the response
+    return response.json()
